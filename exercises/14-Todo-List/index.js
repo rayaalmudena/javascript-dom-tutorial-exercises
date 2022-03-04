d2b03147580e942c7b79a7f7e7e4326cea1f11a3
rayaalmudena <raya.almudena@gmail.com>
2022-03-04 11:44:36 +0100

    14 hay un error si se añade el mismo texto

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+function deleteEvent(elemento) {
+
+    elemento.addEventListener('click', function handleClick(event) {
+        event.target.parentElement.parentElement.classList.add("delete");
+        document.querySelector(".delete").remove();
+    });
+}
+
+const eliminar = document.querySelectorAll("i");
+for (let i = 0; i < eliminar.length; i++) {    
+    deleteEvent(eliminar[i]);   
+}
+
+const addNew = document.querySelector(".todo-header");
+
+addNew.addEventListener('click', (event) => {
+    let nuevoElemento = document.querySelector("#addToDo").value;
+    let insert = '<span><i class="fa fa-trash"></i></span>'+nuevoElemento;
+    let listElement=document.createElement("li");
+    listElement.innerHTML=insert;
+    deleteEvent(listElement);
+    document.querySelector("ul").appendChild(listElement);
+});
